e8c90f1bea91013fd449e3a8527872062b5019d7
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2023-04-08 07:29:06 +0000

    Add support for touch events.

--- src/lib-ui/dashboard/use-dashboard.ts
+++ src/lib-ui/dashboard/use-dashboard.ts
@@ -61,13 +61,14 @@ function useDashboard({ previewId }: DashboardParams) {
       const rowSpan = Number(child.dataset.rowspan)
       const colSpan = Number(child.dataset.colspan)
       const previewLastGridPosition = { col: 0, row: 0 }
+      const touchMovement = { x: 0, y: 0, initialized: false }
 
-      const stopEvent = (event: MouseEvent) => {
+      const stopEvent = (event: Event) => {
         event.stopPropagation()
         event.preventDefault()
       }
-      const onMouseMove = (event: MouseEvent) => {
-        stopEvent(event)
+
+      const onMove = (movement: { y: number; x: number }) => {
         const { x, y, width, height } = child.getBoundingClientRect()
         const { col, row } = getGridPosition(x, y, boundMinX, boundMinY, cellWidth, cellHeight)
 
@@ -101,12 +102,11 @@ function useDashboard({ previewId }: DashboardParams) {
         }
 
         // Move child by mouse movement.
-        child.style.top = `${clamp(y + event.movementY, boundMinY, boundMaxY - height)}px`
-        child.style.left = `${clamp(x + event.movementX, boundMinX, boundMaxX - width)}px`
+        child.style.top = `${clamp(y + movement.y, boundMinY, boundMaxY - height)}px`
+        child.style.left = `${clamp(x + movement.x, boundMinX, boundMaxX - width)}px`
       }
-      const onMouseUp = (_event: MouseEvent) => {
-        document.removeEventListener('mousemove', onMouseMove)
 
+      const onStop = () => {
         // Hide preview
         if (previewRef.current) {
           previewRef.current.style.opacity = '0'
@@ -120,8 +120,8 @@ function useDashboard({ previewId }: DashboardParams) {
         child.style.top = `${boundMinY + cellHeight * row}px`
         child.style.left = `${boundMinX + cellWidth * col}px`
       }
-      child.addEventListener('mousedown', (event) => {
-        stopEvent(event)
+
+      const onStart = () => {
         const { x, y, width, height } = child.getBoundingClientRect()
 
         // Show preview
@@ -138,11 +138,61 @@ function useDashboard({ previewId }: DashboardParams) {
           // Set opacity transition.
           previewRef.current.style.transition = 'opacity 300ms ease-out'
         }
+      }
 
-        // TODO: add support for touch
+      const onMouseMove = (event: MouseEvent) => {
+        stopEvent(event)
+        onMove({ x: event.movementX, y: event.movementY })
+      }
+
+      const onTouchMove = (event: TouchEvent) => {
+        if (event.touches.length !== 1) return
+
+        stopEvent(event)
+        const touch = event.touches.item(0)!
+
+        if (touchMovement.initialized) {
+          onMove({ x: touch.screenX - touchMovement.x, y: touch.screenY - touchMovement.y })
+        }
+
+        touchMovement.initialized = true
+        touchMovement.x = touch.screenX
+        touchMovement.y = touch.screenY
+      }
+
+      const onMouseUp = (_event: MouseEvent) => {
+        document.removeEventListener('mousemove', onMouseMove)
+        document.removeEventListener('mouseup', onMouseUp)
+
+        onStop()
+      }
+
+      const onTouchEnd = (_event: TouchEvent) => {
+        document.removeEventListener('touchmove', onTouchMove)
+        document.removeEventListener('touchend', onTouchEnd)
+
+        onStop()
+      }
+
+      const onMouseDown = (event: MouseEvent) => {
+        stopEvent(event)
+        onStart()
+
         document.addEventListener('mousemove', onMouseMove)
         document.addEventListener('mouseup', onMouseUp)
-      })
+      }
+
+      const onTouchStart = (event: Event) => {
+        stopEvent(event)
+        touchMovement.initialized = false
+        onStart()
+
+        document.addEventListener('touchmove', onTouchMove)
+        document.addEventListener('touchend', onTouchEnd)
+      }
+
+      child.addEventListener('mousedown', onMouseDown)
+      child.addEventListener('ontouchstart', onTouchStart)
     }
   }, [])
 
