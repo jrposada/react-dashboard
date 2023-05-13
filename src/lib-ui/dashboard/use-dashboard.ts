ec188c5a20202dbef3b1252bdd65bb9851703bf8
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2023-05-13 19:05:57 +0200

    Add touch support.

--- src/lib-ui/dashboard/use-dashboard.ts
+++ src/lib-ui/dashboard/use-dashboard.ts
@@ -3,6 +3,11 @@ import clamp from '../math/clamp'
 import getGridPosition from './get-grid-position'
 import updatePositions from './update-positions'
 
+type Movement = {
+  x: number
+  y: number
+}
+
 interface DashboardRefreshParams {
   container: HTMLDivElement | null
   rows: number
@@ -62,12 +67,31 @@ function useDashboard({ previewId }: DashboardParams) {
       const colSpan = Number(child.dataset.colspan)
       const previewLastGridPosition = { col: 0, row: 0 }
 
-      const stopEvent = (event: MouseEvent) => {
+      const stopEvent = (event: Event) => {
         event.stopPropagation()
         event.preventDefault()
       }
-      const onMouseMove = (event: MouseEvent) => {
-        stopEvent(event)
+
+      const onStart = () => {
+        const { x, y, width, height } = child.getBoundingClientRect()
+
+        // Show preview
+        if (previewRef.current) {
+          const { col, row } = getGridPosition(x, y, boundMinX, boundMinY, cellWidth, cellHeight)
+          previewLastGridPosition.col = col
+          previewLastGridPosition.row = row
+
+          previewRef.current.style.opacity = '1'
+          previewRef.current.style.width = `${width}px`
+          previewRef.current.style.height = `${height}px`
+          previewRef.current.style.top = `${boundMinY + cellHeight * row}px`
+          previewRef.current.style.left = `${boundMinX + cellWidth * col}px`
+          // Set opacity transition.
+          previewRef.current.style.transition = 'opacity 300ms ease-out'
+        }
+      }
+
+      const onMove = (movement: Movement) => {
         const { x, y, width, height } = child.getBoundingClientRect()
         const { col, row } = getGridPosition(x, y, boundMinX, boundMinY, cellWidth, cellHeight)
 
@@ -101,12 +125,11 @@ function useDashboard({ previewId }: DashboardParams) {
         }
 
         // Move child by mouse movement.
-        child.style.top = `${clamp(y + event.movementY, boundMinY, boundMaxY - height)}px`
-        child.style.left = `${clamp(x + event.movementX, boundMinX, boundMaxX - width)}px`
+        child.style.top = `${clamp(y + movement.y, boundMinY, boundMaxY - height)}px`
+        child.style.left = `${clamp(x + movement.x, boundMinX, boundMaxX - width)}px`
       }
-      const onMouseUp = (_event: MouseEvent) => {
-        document.removeEventListener('mousemove', onMouseMove)
 
+      const onEnd = () => {
         // Hide preview
         if (previewRef.current) {
           previewRef.current.style.opacity = '0'
@@ -120,29 +143,76 @@ function useDashboard({ previewId }: DashboardParams) {
         child.style.top = `${boundMinY + cellHeight * row}px`
         child.style.left = `${boundMinX + cellWidth * col}px`
       }
+
+      // Mouse support
+      const onMouseMove = (event: MouseEvent) => {
+        stopEvent(event)
+
+        onMove({ x: event.movementX, y: event.movementY })
+      }
+
+      const onMouseUp = (event: MouseEvent) => {
+        stopEvent(event)
+
+        document.removeEventListener('mousemove', onMouseMove)
+        document.removeEventListener('mouseup', onMouseUp)
+
+        onEnd()
+      }
+
       child.addEventListener('mousedown', (event) => {
         stopEvent(event)
-        const { x, y, width, height } = child.getBoundingClientRect()
 
-        // Show preview
-        if (previewRef.current) {
-          const { col, row } = getGridPosition(x, y, boundMinX, boundMinY, cellWidth, cellHeight)
-          previewLastGridPosition.col = col
-          previewLastGridPosition.row = row
+        onStart()
 
-          previewRef.current.style.opacity = '1'
-          previewRef.current.style.width = `${width}px`
-          previewRef.current.style.height = `${height}px`
-          previewRef.current.style.top = `${boundMinY + cellHeight * row}px`
-          previewRef.current.style.left = `${boundMinX + cellWidth * col}px`
-          // Set opacity transition.
-          previewRef.current.style.transition = 'opacity 300ms ease-out'
-        }
-
-        // TODO: add support for touch
         document.addEventListener('mousemove', onMouseMove)
         document.addEventListener('mouseup', onMouseUp)
       })
+
+      // Touch support
+      const touchPosition: Movement = { x: 0, y: 0 }
+      const onTouchMove = (event: TouchEvent) => {
+        console.log('touchmove')
+        stopEvent(event)
+
+        const touch = event.touches.item(0)
+        if (!touch) return
+
+        onMove({ x: touch.clientX - touchPosition.x, y: touch.clientY - touchPosition.y })
+
+        touchPosition.x = touch.clientX
+        touchPosition.y = touch.clientY
+      }
+
+      const onTouchEnd = (event: TouchEvent) => {
+        stopEvent(event)
+
+        document.removeEventListener('touchmove', onTouchMove)
+        document.removeEventListener('touchcancel', onTouchEnd)
+        document.removeEventListener('touchend', onTouchEnd)
+
+        onEnd()
+      }
+
+      child.addEventListener(
+        'touchstart',
+        (event) => {
+          stopEvent(event)
+
+          const touch = event.touches.item(0)
+          if (!touch || touch.target !== child) return
+          console.log('touchstart')
+
+          touchPosition.x = touch.clientX
+          touchPosition.y = touch.clientY
+          onStart()
+
+          document.addEventListener('touchmove', onTouchMove, { passive: false })
+          document.addEventListener('touchcancel', onTouchEnd, { passive: false })
+          document.addEventListener('touchend', onTouchEnd, { passive: false })
+        },
+        { passive: false }
+      )
     }
   }, [])
 
